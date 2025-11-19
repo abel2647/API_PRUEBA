0ca09992ce95e20cbe4278d66ef382798cf0cc30
abel2647 <ahernandez2647@gmail.com>
2025-11-18 20:48:12 -0600

    AlumnoService, registrar() arreglado

--- src/main/java/com/example/fingerprint_api/repositories/AlumnoRepository.java
+++ src/main/java/com/example/fingerprint_api/repositories/AlumnoRepository.java
@@ -22,5 +22,12 @@ public interface AlumnoRepository extends CrudRepository <AlumnoModel,Integer> {
 
     boolean existsByNumeroControl(String numeroControl);
 
-    Object findByCarreraClave(String carreraClave);
+    ArrayList<AlumnoModel> findByCarreraClave(String carreraClave);
+
+    //cambios nuevos
+    // En AlumnoRepository.java
+    long countByCarreraClave(String carreraClave);
+    Optional<AlumnoModel> findByNumeroControlAndUuid(String numeroControl, String uuid);
+    ArrayList<AlumnoModel> findByPrimerNombreContainingIgnoreCaseOrSegundoNombreContainingIgnoreCaseOrApellidoPaternoContainingIgnoreCaseOrApellidoMaternoContainingIgnoreCase(
+            String primerNombre, String segundoNombre, String apellidoPaterno, String apellidoMaterno);
 }
